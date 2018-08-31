7ea6720cc2e7f7899666e190b933ed99809ca5a0
MKachi <mkachi@naver.com>
2018-09-01 02:39:06 +0900

    Add project

---
+++ AM Bot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace AM_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public async Task MainAsync()
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            string token = "";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            client.MessageReceived += MessageReceived;
+            client.Ready += Ready;
+            await Task.Delay(-1);
+        }
+
+        private Task Ready()
+        {
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+        }
+    }
+}
